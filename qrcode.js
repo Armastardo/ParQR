8f43ec1b3adcbd9d41623c02427359ef8b3bff85
Alejandro Colin Varela! <alejandro.colin.varela@gmail.com>
2018-09-08 16:18:02 -0500

    Centrado al fin

--- qrcode.js
+++ qrcode.js
@@ -363,7 +363,6 @@ var QRCode;
 			this._bIsPainted = false;
 			this._elImage = document.createElement("img");
 			this._elImage.alt = "Scan me!";
-			this._elImage.className = "w3-middle";
 			this._elImage.style.display = "none";
 			this._el.appendChild(this._elImage);
 			this._bSupportDataURI = null;
